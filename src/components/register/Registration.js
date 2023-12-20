622b3c88e4c579dd5eb39cc870fec7edfc1bc4ad
arifyourdev <hariskhan855855@gmail.com>
2023-12-21 00:32:01 +0530

    added

--- src/components/register/Registration.js
+++ src/components/register/Registration.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './register.module.css';
+import { db } from "../../firestore.js";
 
 const Registration = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!name) {
+            setError("Name is Required")
+            return;
+        }
+        if (!email) {
+            setError("Email is Required")
+            return
+        }
+        if (!password) {
+            setError("Password is Required")
+            return
+        }
+        try {
+
+        } catch (e) {
+            setError("Error in Submiting Data")
+        }
+        setError("")
+
+    }
     return (
         <div className={style.formContainer}>
-            <form className={style.LoginPage_form}>
+            <form className={style.LoginPage_form} onSubmit={handleSubmit}>
                 <h2 className={style.loginTitle}>Sign Up</h2>
-                <input type="text" name="name" className={style.loginInput} placeholder="Enter Name" />
-                <input type="email" name="email" className={style.loginInput} placeholder="Enter Email" />
-                <input type="password" name="password" className={style.loginInput} placeholder="Enter Password" />
-                <button className={style.loginBtn}>Sign In</button>
+                <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} className={style.loginInput} placeholder="Enter Name" />
+                <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className={style.loginInput} placeholder="Enter Email" />
+                <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} className={style.loginInput} placeholder="Enter Password" />
+                {error && <span style={{ color: "red" }}>{error}</span>}
+                <button className={style.loginBtn} type="submit">Sign In</button>
                 <a className={style.anchor_color} href="/login" >
                     <p>Or SignIn instead</p>
                 </a>
---
+++ src/firestore.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbALBP546TurUlSXxCE-qxozcRxepit38",
+    authDomain: "buybusyone.firebaseapp.com",
+    databaseURL: "https://buybusyone-default-rtdb.firebaseio.com",
+    projectId: "buybusyone",
+    storageBucket: "buybusyone.appspot.com",
+    messagingSenderId: "523216915762",
+    appId: "1:523216915762:web:613a7c54419ec3fafddb2b",
+    measurementId: "G-Y94J6C22L9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
